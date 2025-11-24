f7d304762841c66ec356cb07a8263bf2a63079ed
NikitaAksenov <naksenov96@gmail.com>
2025-11-24 08:37:12 +0300

    Added commands router

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/NikitaAksenov/roadmap-task_tracker/internal/router"
 	"github.com/NikitaAksenov/roadmap-task_tracker/internal/storage"
 )
 
 type application struct {
+	Router  router.Router
 	Storage storage.Storage
 }
 
 func main() {
 	app := &application{
+		Router:  *router.NewRouter(),
 		Storage: storage.NewStorage(),
 	}
 
@@ -24,16 +27,7 @@ func main() {
 	command := os.Args[1]
 	args := os.Args[2:]
 
-	router := make(map[string]func([]string))
-	router["add"] = app.commandAdd
-	router["update"] = app.commandUpdate
-	router["delete"] = app.commandDelete
-	router["list"] = app.commandList
-	router["mark"] = app.commandMark
+	app.prepareCommands()
 
-	if commandFunc, ok := router[command]; ok {
-		commandFunc(args)
-	} else {
-		fmt.Printf("Unknown command [%s]\n", command)
-	}
+	app.Router.Execute(command, args)
 }
---
+++ internal/router/router.go
@@ -0,0 +1,40 @@
+package router
+
+import "errors"
+
+var (
+	ErrCommandAlreadyExists = errors.New("command already exists")
+	ErrCommandNotExists     = errors.New("command not exist")
+)
+
+type Router struct {
+	commands map[string]func([]string)
+}
+
+func NewRouter() *Router {
+	r := &Router{
+		commands: make(map[string]func([]string)),
+	}
+
+	return r
+}
+
+func (r *Router) AddCommand(commandName string, command func([]string)) error {
+	if _, ok := r.commands[commandName]; ok {
+		return ErrCommandAlreadyExists
+	}
+
+	r.commands[commandName] = command
+
+	return nil
+}
+
+func (r *Router) Execute(commandName string, args []string) error {
+	if command, ok := r.commands[commandName]; ok {
+		command(args)
+
+		return nil
+	}
+
+	return ErrCommandNotExists
+}
